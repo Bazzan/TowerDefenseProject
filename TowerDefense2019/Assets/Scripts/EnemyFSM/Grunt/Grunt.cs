3be5539c79396a061460a7da391f51635a8c786b
Bazzan <sebake01@gmail.com>
2019-06-13 20:43:00 +0200

    Nya mappar, Enemy FSM

---
+++ TowerDefense2019/Assets/Scripts/EnemyFSM/Grunt/Grunt.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Grunt : EnemyStateMachine
+{
+
+
+    [Header("Attributes")]
+    [SerializeField] public float speed = 10f;
+    [SerializeField] public int health = 100;
+    [SerializeField] public int value = 50;
+    [SerializeField] public int dmgCastel;
+
+    
+    [Header("NavMesh")]
+    [SerializeField] public NavMeshAgent Agent;
+    [SerializeField] public Transform start;
+    [SerializeField] public Transform EndDestination;
+    [SerializeField] public bool AgentHasPath = false;
+    //[SerializeField] public NavMeshPath path;
+
+
+    [Header("VFX")]
+    [SerializeField] public GameObject deathEffect;
+
+
+    [Header("Other")]
+    [HideInInspector] public float dist;
+    [HideInInspector] public LineRenderer LineRend;
+
+
+    protected override void Awake()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        LineRend = GetComponent<LineRenderer>();
+        //path = new NavMeshPath();
+
+        base.Awake();
+        Debug.Log("Grunt.cs " + currentState.ToString());
+
+    }
+
+
+    
+}
+
---
+++ TowerDefense2019/Assets/Scripts/EnemyFSM/Grunt/GruntBaseState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "Enemy/Grunt/GruntBaseState")]
+public class GruntBaseState : EnemyState
+{
+
+
+
+    protected Grunt Owner;
+
+    public override void Initialize(EnemyStateMachine Owner)
+    {
+        this.Owner = (Grunt)Owner;
+    }
+
+
+    public override void Enter()
+    {
+        Debug.Log("GruntBaseState.cs -> Enter");
+    }
+
+    //public override void HandleUpdate()
+    //{
+        
+    //}
+
+
+}
